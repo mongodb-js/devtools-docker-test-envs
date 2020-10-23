f73101eb418718e62991a03f2749c17b67958c77
mcasimir <maurizio.cas@gmail.com>
2020-10-23 22:09:56 +0200

    Add add-version script

---
+++ enterprise/add-version.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+cd "$(dirname "$0")"
+
+mkdir "$1"
+cd "$1"
+curl -O --remote-name-all "https://raw.githubusercontent.com/docker-library/mongo/master/$1/{Dockerfile,docker-entrypoint.sh}"
+
+chmod 755 ./docker-entrypoint.sh
+
+HEAD=`head -1 Dockerfile`
+TAIL=`tail -n +2 Dockerfile`
+
+echo "${HEAD}" > Dockerfile
+
+#
+# Add to the build here:
+#
+echo '
+RUN apt-get update -y && apt-get install libsasl2-modules-gssapi-mit
+' >> Dockerfile
+
+echo "${TAIL}" >> Dockerfile
