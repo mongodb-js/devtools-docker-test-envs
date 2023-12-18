d002731185da651c62c17115ba83937eb20b1990
Rhys Howell <rhys.howell@mongodb.com>
2023-12-18 16:01:13 -0500

    start mongodb with auth enabled to ensure we're testing it correctly

--- docker/oidc/mock-oidc-provider/start-server.sh
+++ docker/oidc/mock-oidc-provider/start-server.sh
@@ -8,10 +8,42 @@ echo Waiting to make sure that oidc mock provider and proxy are running
 until $(curl --output /dev/null --silent --head --fail http://localhost:$OIDC_PROVIDER_PROXY_PORT/.well-known/openid-configuration); do
     sleep 0.3
 done
+
+echo Setting up user roles
+# Start the server (without auth).
+# This is original mongodb/mongodb-enterprise-server entrypoint
+python3 /usr/local/bin/docker-entrypoint.py \
+    --setParameter authenticationMechanisms="MONGODB-OIDC" \
+    --setParameter enableTestCommands="true" \
+    --setParameter oidcIdentityProviders="$OIDC_IDENTITY_PROVIDERS" > /dev/null &
+MDB_PID="$!"
+
+# Wait for the mongodb server to start.
+# sleep 5
+until nc -z localhost 27017; do
+    sleep 1
+done
+
+# Creates the OIDC user role in the database.
+mongosh "mongodb://localhost:27017/admin" --eval "JSON.stringify(db.createRole({ role: \"dev/groups\", privileges: [ ], roles: [ \"dbOwner\" ] }));"
+
+# Stop the no auth database (we re-start it with auth enabled next).
+echo Stopping no-auth server pid $MDB_PID
+kill $MDB_PID
+
+pkill mongod
+
+# Wait for the mongodb server to shut down.
+# sleep 15
+until ! nc -z localhost 27017; do
+    sleep 1
+done
+
 echo Starting server
 OIDC_IDENTITY_PROVIDERS="[$(curl --fail http://localhost:29091/server-oidc-config)]"
 # This is original mongodb/mongodb-enterprise-server entrypoint
 python3 /usr/local/bin/docker-entrypoint.py \
-    --setParameter authenticationMechanisms="SCRAM-SHA-256,MONGODB-OIDC" \
+    --setParameter authenticationMechanisms="MONGODB-OIDC" \
     --setParameter enableTestCommands="true" \
+    --auth \
     --setParameter oidcIdentityProviders="$OIDC_IDENTITY_PROVIDERS"
